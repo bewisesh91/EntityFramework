248f8f1eed3613a1546227e5ffdc65b7ba55f779
Seunghyun <bewise.seunghyun@gmail.com>
2023-01-24 14:44:33 +0900

    Firtst

---
+++ SampleEntityFramework/Models/Author.cs
@@ -0,0 +1,11 @@
+﻿namespace SampleEntityFramework.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Gender { get; set; }
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
